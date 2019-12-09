8ad49a0fe11d2c6774b45561406db6d12dd57aec
zhangsan <zhangsan@gmail.com>
2019-12-09 20:47:17 +0800

    Failed to finish homework of course 4.

--- nowtagram/views.py
+++ nowtagram/views.py
@@ -11,8 +11,28 @@ from flask_login import login_user, logout_user, login_required, current_user
 # 首页
 @app.route('/')
 def index():
-    images = Image.query.order_by(Image.id.desc()).limit(10).all()
-    return render_template('index.html', images=images)
+    # images = Image.query.order_by(Image.id.desc()).limit(10).all()
+    # return render_template('index.html', images=images)
+
+    # 实现主页的AJAX分页显示
+    paginate = Image.query.paginate(page=1, per_page=10, error_out=False)
+    return render_template('index.html', images=paginate.items, has_next=paginate.has_next)
+
+
+# 分页显示主页的图片，点更多动态加载图片
+@app.route('/index/images/<int:page>/<int:per_page>/')
+def index_images(page, per_page):
+    paginate = Image.query.paginate(page=page, per_page=per_page, error_out=False)
+    v_map = {'has_next': paginate.has_next}
+    images = []
+    for image in paginate.items:
+        # 主页json格式如何处理？传入的这3个变量是如何处理的？
+        imgvo = {'ct_date': image.created_date, 'id': image.id, 'url': image.url, 'comment_count': len(image.comments),
+                 'comments': image.comments, 'u_id': image.user.id, 'u_head_url': image.user.head_url,
+                 'u_username': image.user.username}
+        images.append(imgvo)
+    v_map['images'] = images
+    return json.dumps(v_map)
 
 
 # 图片详情页
@@ -20,10 +40,11 @@ def index():
 @app.route('/image/<int:image_id>/')
 def image(image_id):
     # 由index里查询的图片id来查找相应的图片
-    image = Image.query.get(image_id)
-    if image == None:
+    v_image = Image.query.get(image_id)
+    # 用is代替==
+    if v_image is None:
         return redirect('/')
-    return render_template('pageDetail.html', image=image)
+    return render_template('pageDetail.html', image=v_image)
 
 
 # 个人详情页
@@ -31,7 +52,7 @@ def image(image_id):
 @login_required     # 只有登录才能查看
 def profile(user_id):
     user = User.query.get(user_id)
-    if user == None:
+    if user is None:
         return redirect('/')
 
     # 分页显示用户的图片
@@ -44,16 +65,16 @@ def profile(user_id):
 def user_images(user_id, page, per_page):
     paginate = Image.query.filter_by(user_id=user_id).paginate(page=page, per_page=per_page, error_out=False)
     # 判断是否还有下一页，是否需要显示更多按钮，paginate.has_next参数
-    map = {'has_next': paginate.has_next}
+    v_map = {'has_next': paginate.has_next}
     # 返回的图片信息也放到map里
     images = []
     for image in paginate.items:
         imgvo = {'id': image.id, 'url': image.url, 'comment_count': len(image.comments)}
         images.append(imgvo)
-    map['images'] = images
+    v_map['images'] = images
 
     # 返回json格式的数据，用json.dumps而不是json.dump，然后靠前端处理相应的数据
-    return json.dumps(map)
+    return json.dumps(v_map)
 
 
 # 注册登录页
@@ -71,7 +92,7 @@ def regloginpage():
 
 # 因为要闪现很多信息，需包装成函数
 def redirect_with_msg(target, msg, category):
-    if msg != None:
+    if msg is not None:
         # category指信息来自哪里
         flash(msg, category=category)
     return redirect(target)
@@ -88,7 +109,7 @@ def reg():
         return redirect_with_msg('/regloginpage/', '用户名或密码不能为空', category='reglogin')
 
     user = User.query.filter_by(username=username).first()
-    if user != None:
+    if user is not None:
         return redirect_with_msg('/regloginpage/', '用户名已存在', category='reglogin')
 
     # 更多判断
@@ -109,7 +130,7 @@ def reg():
     login_user(user)
 
     next = request.values.get('next')
-    if next != None and next.startswith('/'):
+    if next is not None and next.startswith('/'):
         return redirect(next)
 
     return redirect('/')
@@ -125,7 +146,7 @@ def login():
         return redirect_with_msg('/regloginpage/', '用户名或密码不能为空', category='reglogin')
 
     user = User.query.filter_by(username=username).first()
-    if user == None:
+    if user is None:
         return redirect_with_msg('/regloginpage/', '用户不存在', category='reglogin')
 
     m = hashlib.md5()
@@ -137,7 +158,7 @@ def login():
 
     # 需要经由login.html的表单form提交再在此处获取，因为login链接本来就是在表单form跳转到这的
     next = request.values.get('next')
-    if next != None and next.startswith('/'):
+    if next is not None and next.startswith('/'):
         return redirect(next)
 
     return redirect('/')
@@ -148,8 +169,3 @@ def login():
 def logout():
     logout_user()
     return redirect('/')
-
-
-
-
-
