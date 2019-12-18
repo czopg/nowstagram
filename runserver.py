6f5270c745b7c5ea8e67861b47446121f1d42282
czopg <49984587+czopg@users.noreply.github.com>
2019-12-18 11:14:52 +0800

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,7 +1,8 @@
 # -*- encoding=UTF-8 -*-
 
 from nowstagram import app
-
+from werkzeug.contrib.fixers import ProxyFix
 
 if __name__ == '__main__':
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(debug=True)
